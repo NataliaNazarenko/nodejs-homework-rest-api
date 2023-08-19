7fd57ddf659f3e2a6f561cbc89858d37326b4a19
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-08-19 22:51:52 +0300

    add register/login/logout

--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,99 @@
-const bcrypt = require('bcrypt')
-const { User } = require('../models/user')
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const { User } = require('../models/user');
 const { ctrlWrapper, HttpError } = require('../helpers');
+const { SECRET_KEY } = process.env;
+
 
 const register = async (req, res) => {
+
     const {email, password} = req.body;
-    const user = await User.findOne(email);
+    const user = await User.findOne({email});
 
     if(user){
         throw HttpError (409, "Email already in use")
     }
 
     const hashPassword = await bcrypt.hash(password, 10)
-
-    const newUser = await User.create({...req.body, password: hashPassword});
+    
+    const newUser = await User.create({
+        ...req.body,
+        password: hashPassword,
+    });
 
     res.status(201).json({
         email: newUser.email,
+        subscription: newUser.subscription,
     })
 
 }
 
+const login = async (req, res) => {
+
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw HttpError (401, "Email or password invalid")
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+
+    if(!passwordCompare){
+        throw HttpError (401, "Email or password invalid")
+    }
+
+    const payload = {
+        id: user._id,
+    }
+
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+    await User.findByIdAndUpdate(user._id, {token});
+
+    res.json({
+        token,
+    })
+
+}
+
+const getCurrent = async (req, res) => {
+    const {email} = req.user;
+
+    res.json({
+        email,
+    })
+}
+
+const logout = async (req, res) => {
+    const {_id} = req.user;
+    const user = await User.findById(_id);
+
+    if (!user) {
+        return res.status(401).json({ message: 'Not authorized' });
+    }
+
+    await User.findByIdAndUpdate(_id, {token: ''});
+   
+    res.json({
+        message: 'Logout success'
+    })
+
+}
+
+const updateSubscription = async (req, res) => {
+    const { _id } = req.user;
+    const { subscription } = req.body;
+      
+    await User.findByIdAndUpdate(_id, { subscription });
+    res.status(200).json({ subscription });
+   
+  };
+
 module.exports = {
     register: ctrlWrapper(register),
-    
-  };
+    login: ctrlWrapper(login),
+    getCurrent: ctrlWrapper(getCurrent),
+    logout: ctrlWrapper(logout),
+    updateSubscription: ctrlWrapper(updateSubscription),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,19 +2,19 @@ const express = require('express')
 const {schemas} = require('../../models/contact')
 const router = express.Router()
 const ctrl = require('../../controllers/contacts')
-const {validateBody, isValidId} = require('../../middlewares')
+const {validateBody, isValidId, authenticate} = require('../../middlewares')
 
 
-router.get('/', ctrl.listContacts)
+router.get('/', authenticate, ctrl.listContacts)
 
-router.get('/:id', isValidId, ctrl.getContactById)
+router.get('/:id', authenticate, isValidId, ctrl.getContactById)
 
-router.post('/', validateBody(schemas.addSchema), ctrl.addContact)
+router.post('/', authenticate, validateBody(schemas.addSchema), ctrl.addContact)
 
-router.delete('/:id', isValidId, ctrl.removeContact)
+router.delete('/:id', authenticate, isValidId, ctrl.removeContact)
 
-router.put('/:id', isValidId, validateBody(schemas.addSchema), ctrl.updateContact)
+router.put('/:id', authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateContact)
 
-router.patch('/:id/favorite', isValidId, validateBody(schemas.updateFavoriteSchemas), ctrl.updateStatusContact)
+router.patch('/:id/favorite', authenticate, validateBody(schemas.updateFavoriteSchemas), ctrl.updateStatusContact)
 
 module.exports = router
